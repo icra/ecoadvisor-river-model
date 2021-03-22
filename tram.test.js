a838d566cf8317dc0f2401fb46025a5481b33edd
Lluís Bosch <holalluis@gmail.com>
2021-03-23 00:07:53 +0100

    afegir test per metode calcula_Di_a_partir_de_Qi

--- tram.test.js
+++ tram.test.js
@@ -5,7 +5,10 @@ Tram = require("./tram.js"); //class
 console.log(Tram.info);
 
 //test valors Vicenç Acuña (vacuna@icra.cat)
-//syntax:          (wb, wt, Db,     S,      n,   Li,  Di)
-let tram = new Tram( 3,  6,  2, 0.005, 0.0358, 1000, 1.2);
+//syntax-----------(wb, wt, Db, S,     n,      Li,   Di)
+let tram = new Tram(3,  6,  2,  0.005, 0.0358, 1000, 1.2);
 console.log(tram);
 console.log(tram.resultats);
+
+//test calcular fondaria
+console.log(tram.calcula_Di_a_partir_de_Qi(1000));
